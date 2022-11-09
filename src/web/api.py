b89f9e238e6c3e8c089c5830c7f49367d3d3ba65
Shawn Duong <shawnduong@protonmail.com>
2022-11-09 05:41:43 -0800

    Implemented vulnerable SQL lookup.

--- src/web/api.py
+++ src/web/api.py
@@ -2,8 +2,9 @@ from app import *
 
 import subprocess
 
-@app.route("/api", methods=["POST"])
-def vuln():
+@app.route("/grep", methods=["POST"])
+@login_required
+def vuln_grep():
 	"""
 	Vulnerable API endpoint literally executes whatever commands you POST to it
 	and then returns the output.
@@ -15,6 +16,23 @@ def vuln():
 		output = {"result": subprocess.check_output(command, shell=True).decode("utf-8")}
 	except subprocess.CalledProcessError as err:
 		output = {"result": f"Command: {command} failed."}
+
+	return output
+
+@app.route("/sql", methods=["POST"])
+@login_required
+def vuln_sql():
+	"""
+	Vulnerable API endpoint may leak a database.
+	"""
+
+	sql = f"SELECT * FROM logs WHERE date=\"{request.json['command']}\";"
+
+	try:
+		result = db.session.execute(sql).all()
+		output = {"result": "\n".join([str(r) for r in result])}
+	except Exception as err:
+		output = {"result": f"Query failed: {err}"}
 
 	return output
 
--- src/web/static/js/app.js
+++ src/web/static/js/app.js
@@ -1,8 +1,17 @@
 /* This is the send button (found by ID). */
 const send = document.getElementById("send-grep");
 
+/* Same but for SQL. */
+const sendSQL = document.getElementById("send-sql");
+
 /* Override default form behavior so it doesn't take us to a new page. */
-form.addEventListener("submit", (event) =>
+formA.addEventListener("submit", (event) =>
+{
+	event.preventDefault();
+});
+
+/* Override default form behavior so it doesn't take us to a new page. */
+formB.addEventListener("submit", (event) =>
 {
 	event.preventDefault();
 });
@@ -15,7 +24,7 @@ send.addEventListener("click", (event) =>
 
 	/* Prepare to POST the data to the API endpoint. */
 	let request = new XMLHttpRequest();
-	request.open("POST", "/api", true);
+	request.open("POST", "/grep", true);
 	request.setRequestHeader("Content-Type", "application/json");
 
 	/* Send the data. */
@@ -34,4 +43,33 @@ send.addEventListener("click", (event) =>
 	/* Find the output section and add the output to it. */
 	document.getElementById("output-grep").innerHTML = "";
 	document.getElementById("output-grep").appendChild(tmp);
+});
+
+/* Get the sent command and run it. */
+sendSQL.addEventListener("click", (event) =>
+{
+	/* Get the command the user sent. */
+	let command = document.getElementById("command-sql").value;
+
+	/* Prepare to POST the data to the API endpoint. */
+	let request = new XMLHttpRequest();
+	request.open("POST", "/sql", true);
+	request.setRequestHeader("Content-Type", "application/json");
+
+	/* Send the data. */
+	let data = {"command": command};
+	request.send(JSON.stringify(data));
+
+	let tmp = document.createElement("p");
+
+	/* Display the output on the page. */
+	request.onload = function()
+	{
+		let response = JSON.parse(this.responseText);
+		tmp.innerHTML = response["result"];
+	}
+
+	/* Find the output section and add the output to it. */
+	document.getElementById("output-sql").innerHTML = "";
+	document.getElementById("output-sql").appendChild(tmp);
 });
